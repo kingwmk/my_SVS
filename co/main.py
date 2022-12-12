a677f287a0af495b47dc6232fc27012249330465
kingwmk <1171147450@qq.com>
2022-12-12 21:11:24 +0800

    Update main.py

--- co/main.py
+++ co/main.py
@@ -1,9 +1,10 @@
 
-from . import colmap
+from co.colmap import Colmap
 
 colmap_path = "/home/hadoop-pnc/wangmingkun/software/colmap/bin/colmap"
 working_dir = "/home/hadoop-pnc/wangmingkun/datasets/MVS/theOldGate"
-im_paths = ["/home/hadoop-pnc/wangmingkun/datasets/MVS/theOldGate/images",]
+im_root= "/home/hadoop-pnc/wangmingkun/datasets/MVS/theOldGate/images"
+im_paths = sorted(os.listdir(im_root))
 colmap = Colmap(colmap_path, working_dir, im_paths)
 colmap.sparse_reconstruction_unknown_calib()
 #colmap.sparse_reconstruction_known_calib(Ks, Ts, imdims)
