dce26407c3f2168db765e7e0c5ffa3bcf54621b9
kingwmk <1171147450@qq.com>
2022-12-12 20:15:18 +0800

    Update main.py

--- co/main.py
+++ co/main.py
@@ -1,5 +1,5 @@
 import colmap
 colmap = Colmap(colmap_path, working_dir, im_paths)
-#colmap.sparse_reconstruction_unknown_calib()
-colmap.sparse_reconstruction_known_calib(Ks, Ts, imdims)
+colmap.sparse_reconstruction_unknown_calib()
+#colmap.sparse_reconstruction_known_calib(Ks, Ts, imdims)
 colmap.dense_reconstruction()
